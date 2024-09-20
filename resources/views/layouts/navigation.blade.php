4880f658b17173406ff50c0b891111e983878fb3
juit-cop <un.sospiro.chromatique@gmail.com>
2024-09-20 20:30:44 +0900

    search

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -22,6 +22,9 @@
           <x-nav-link :href="route('tweets.create')" :active="request()->routeIs('tweets.create')">
             {{ __('Tweet作成') }}
           </x-nav-link>
+          <x-nav-link :href="route('tweets.search')" :active="request()->routeIs('tweets.search')">
+            {{ __('Tweet検索') }}
+          </x-nav-link>
         </div>
       </div>
 
@@ -82,6 +85,9 @@
       </x-responsive-nav-link>
       <x-responsive-nav-link :href="route('tweets.create')" :active="request()->routeIs('tweets.create')">
         {{ __('Tweet作成') }}
+      </x-responsive-nav-link>
+      <x-responsive-nav-link :href="route('tweets.search')" :active="request()->routeIs('tweets.search')">
+        {{ __('Tweet検索') }}
       </x-responsive-nav-link>
     </div>
 
